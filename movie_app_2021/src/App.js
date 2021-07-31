e7a1500895021360631682722f253eb8f967c5e4
Sua Kim <sua99831@ajou.ac.kr>
2021-07-31 23:04:29 +0900

    first commit

---
+++ movie_app_2021/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Potato(){
+  return <h1>I like Potato</h1>;
+}
+
+function App(){
+  return(
+    <div>
+      <h1>Hello</h1>
+      <Potato />
+    </div>
+  );
+}
+
+export default App;
